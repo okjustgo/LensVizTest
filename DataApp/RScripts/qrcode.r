9a162bed558748967b07e70da370bf7cd75f8cf5
Justin Slone <jslone@windows.microsoft.com>
2016-07-26 15:18:45 -0700

    fixing R code

--- DataApp/RScripts/qrcode.r
+++ DataApp/RScripts/qrcode.r
@@ -1,7 +1,18 @@
 library(qrcode)
-ms <- as.character(as.integer(as.POSIXct(Sys.time())))
-url <- 'http://deftgeneralstorage.blob.core.windows.net/holograph/irisTest_test.hgd'
-key <- '<>'
-code <- paste(ms, url, key, sep='|')
+type <- 'auth'
+url <- 'deftgeneralstorage'
+key <- 'YHsWGGl3lYDhFRGkkDxp1JvHsrf5tp9ySfIjPZZ75KZlxuiLDT0LBILsVr/Xw0H0yIHMDDnMMYmWBtcGaH2+Sw=='
+code <- paste(type, url, key, sep='|')
 code
-qrcode_gen(code)
+#qrcode_gen(code)
+qrcode_gen(code,softLimitFlag = TRUE)
+
+type <- 'graph'
+ms <- as.character(as.integer(as.POSIXct(Sys.time())))
+container <- 'holograph'
+blob <- 'irisTest_test.hgd'
+code <- paste(type, ms, container, blob, sep='|')
+code
+#qrcode_gen(code)
+qrcode_gen(code,softLimitFlag = TRUE)
+
