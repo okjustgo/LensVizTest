bc76b4a7e3ccf33daea070a3aad5c795703176bb
Justin Slone <jslone@microsoft.com>
2016-11-14 17:26:18 -0800

    Adding holotoolkit update

--- LenVizTest/Assets/HoloToolkit/Utilities/Editor/BuildCommands.cs
+++ LenVizTest/Assets/HoloToolkit/Utilities/Editor/BuildCommands.cs
@@ -1,67 +1,20 @@
-﻿using System.Linq;
-using UnityEditor;
-using UnityEngine;
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+//
 
-namespace HoloToolkit.Unity
+using HoloToolkit.Unity;
+
+/// <summary>
+/// Implements functionality for building HoloLens applications
+/// </summary>
+public static class HoloToolkitCommands
 {
     /// <summary>
-    /// Implements functionality for building HoloLens applications
+    /// Do a build configured for the HoloLens, returns the error from BuildPipeline.BuildPlayer
     /// </summary>
-    public static class BuildCommands
+    public static bool BuildSLN()
     {
-        public static readonly string BuildLocation = "Builds/HoloLens";
-
-        struct BuildSettings
-        {
-            public BuildTarget BuildTarget;
-            public WSASDK WSASDK;
-            public WSAUWPBuildType WSAUWPBuildType;
-
-            public static BuildSettings Current
-            {
-                get
-                {
-                    return new BuildSettings()
-                    {
-                        BuildTarget = EditorUserBuildSettings.activeBuildTarget,
-                        WSASDK = EditorUserBuildSettings.wsaSDK,
-                        WSAUWPBuildType = EditorUserBuildSettings.wsaUWPBuildType
-                    };
-                }
-            }
-
-            public void Apply()
-            {
-                EditorUserBuildSettings.wsaSDK = WSASDK;
-                EditorUserBuildSettings.wsaUWPBuildType = WSAUWPBuildType;
-                EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTarget);
-            }
-        }
-
-        /// <summary>
-        /// Do a build configured for the HoloLens, returns the error from BuildPipeline.BuildPlayer
-        /// </summary>
-        public static string BuildForHololens()
-        {
-
-            // Cache the current settings
-            BuildSettings oldBuildSettings = BuildSettings.Current;
-
-            // Define and apply the desired settings
-            BuildSettings newBuildSettings = oldBuildSettings;
-            newBuildSettings.BuildTarget = BuildTarget.WSAPlayer;
-            newBuildSettings.WSASDK = WSASDK.UWP;
-            newBuildSettings.WSAUWPBuildType = WSAUWPBuildType.D3D;
-            newBuildSettings.Apply();
-
-            // Capture the active scenes, and build
-            var scenes = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path);
-            string error = BuildPipeline.BuildPlayer(scenes.ToArray(), BuildLocation, newBuildSettings.BuildTarget, BuildOptions.None);
-
-            // Restore old build settings
-            oldBuildSettings.Apply();
-
-            return error;
-        }
+        return BuildDeployTools.BuildSLN(BuildDeployPrefs.BuildDirectory, false);
     }
 }
